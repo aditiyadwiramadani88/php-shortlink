46f7fb127a42210810ab1a5ea28b60ca5c3a5f26
aditiyadwiramadani88 <aditiyadwiramadani88@gmail.com>
2021-07-24 16:56:57 +0700

    templates

---
+++ Link.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Short Link</title>
+    <link rel="stylesheet" href="/static/style.css">
+</head>
+<body>
+
+    <div class="container">
+        <div class="title">
+            <h1>URl Short Link</h1>
+        </div>
+    <form method="POST">
+
+            <label for="link">Paste Link</label>
+            <input type="text" name="url" id="link" autocomplete="off">
+
+            <div class="submit">
+                <button type="submit">Submit</button>
+            </div>
+           
+        
+    </form>
+    </div>
+    
+    <script src="/static/main.js"></script>
+</body>
+</html>
