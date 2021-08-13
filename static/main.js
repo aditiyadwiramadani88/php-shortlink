dc5740e93fa02a7bcc500762595cc6358a5573ec
aditiyadwiramadani88 <aditiyadwiramadani88@gmail.com>
2021-08-13 11:57:03 +0700

    add copy text

--- static/main.js
+++ static/main.js
@@ -1,20 +1,19 @@
 const link = document.querySelector('#link')
 const form = document.querySelector('form')
 const label = document.querySelector('label')
+const button = document.querySelector('button')
 
 // submit event 
 form.addEventListener('submit', (e) => {
 
-    e.preventDefault()
-    
+    e.preventDefault()    
     // post data 
     async function get_data() { 
     let response  = await fetch('/get', {
     method: "POST", 
     body: new FormData(form)
     })
-
-return response.json()
+    return response.json()
 }
 
 get_data().then((data) => {
@@ -24,14 +23,24 @@ get_data().then((data) => {
    
 }else { 
     
+    
     link.style.border = "2px solid #4CAF50"
     link.value = location.href + data['data']
     label.innerHTML = "Your Link"
+    button.innerHTML = "copy"
+
+    // copy text 
+    button.addEventListener('click', () => {
+
+        link.select()      
+        link.setSelectionRange(0,99999)
+        document.execCommand('copy')
+        document.location.href = '/'
+        alert('Copied the link')
+
+    })
       
     }
  })
-
-
-
 
   })
